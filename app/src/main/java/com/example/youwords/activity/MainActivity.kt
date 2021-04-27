68696a1812f8702815be6d4731c885915542f874
Al <al@example.com>
2021-04-27 23:08:53 +0300

    кнопки BottomAppBar работают, а навигация внутри фрагментов поломалась!!

--- app/src/main/java/com/example/youwords/activity/MainActivity.kt
+++ app/src/main/java/com/example/youwords/activity/MainActivity.kt
@@ -3,49 +3,82 @@ package com.example.youwords.activity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
+import androidx.fragment.app.*
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import com.example.youwords.R
 import com.example.youwords.allwords.AllWordsFragment
+import com.example.youwords.search_and_found.SearchFragment
+import com.example.youwords.start.StartFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     private lateinit var navController: NavController
     private lateinit var toolbar: Toolbar
     private lateinit var bottomNavigationView: BottomNavigationView
-    lateinit var k:AllWordsFragment
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
+
+        //setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
        // setupActionBarWithNavController(navController)
         toolbar=findViewById(R.id.toolbar)
-        //bottomNavigationView.background.current
+
         setSupportActionBar(toolbar)
-     //   bottomNavigationView.background=null
+
         bottomNavigationView=findViewById(R.id.bottomNavigationView)
+        lateinit  var  sF: Fragment
+
         bottomNavigationView.background=null
+       val  startF=StartFragment()
+        //supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, startF).commit()
+        //    bottomNavigationView.background=null
+      //  val g:AllWordsFragment
+       // supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_container, startF).commit()
 
-        bottomNavigationView.setOnNavigationItemReselectedListener {
-       when (it.itemId){
-           R.id.search->  Toast.makeText(this, "List", Toast.LENGTH_LONG).show()
+        val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    val fragment = SearchFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, fragment.javaClass.getSimpleName())
+                            .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.list -> {
+                    val fragment =StartFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, fragment, fragment.javaClass.getSimpleName())
+                            .commit()
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
 
-        } }
-
-
-
+fun listner() {///// этот метод работает не так-по второму клику кнопки бара
+    bottomNavigationView.setOnNavigationItemReselectedListener {
+        Toast.makeText(this, " is clicked", Toast.LENGTH_SHORT).show()
+        when (it.itemId) {
+            R.id.search -> sF = SearchFragment()
+        }
+        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment, sF).commit()
     }
+}
+
+}
 
  //   override fun onSupportNavigateUp(): Boolean {
  //       return navController.navigateUp() || super.onSupportNavigateUp()
@@ -58,6 +91,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
        val id=item.itemId
+
        if(id==R.id.menu)
        {     Toast.makeText(this, "MENU! ", Toast.LENGTH_LONG).show()}
      return super.onOptionsItemSelected(item)
